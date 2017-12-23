728179c80d40d02110787452b28d462e69e7699d
Petr Schütz <petr.schutz@unicorn.eu>
2017-12-23 23:58:08 +0100

    Initial commit

---
+++ dfs.h
@@ -0,0 +1,16 @@
+//
+// Created by Petr Schutz - A16B0130P on 23.12.2017.
+//
+
+#ifndef SEM_DFS_H
+#define SEM_DFS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node {
+    struct node *next;
+    int vertex;
+} node;
+
+#endif //SEM_DFS_H
